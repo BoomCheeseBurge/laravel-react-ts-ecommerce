b157b8faadfd03f4a60ffb988f1cc773636ca969
Brandon John <80831628+BoomCheeseBurge@users.noreply.github.com>
2025-02-09 21:48:16 +0700

    Installed and configured Filament admin panel

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Enums\RolesEnum;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Route;
+use App\Http\Requests\Auth\LoginRequest;
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -27,12 +29,29 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request): HttpFoundationResponse
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
+        /**
+         * @var App\Models\User $user
+         */
+        $user = auth()->user();
+
+        if ($user->hasAnyRole([RolesEnum::Admin, RolesEnum::Vendor])) {
+
+            // Redirect to outside of the Inertia page which is Filament's admin panel page
+            return Inertia::location(route('filament.admin.pages.dashboard'));
+        }
+
+        /**
+         * Absolute false means to generate URL relative to the current URL
+         * In other words, the full base URL is not generated (e.g., 'http://laravel-react-ts-ecommerce.test/dashboard') 
+         * Instead, it will only be '/dashboard'
+         * The full base URL will come from the redirect intended route.
+         */
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
