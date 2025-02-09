afa8e94ee4ec9fd25ba4a19dd0e64477e78f9bfd
Brandon John <80831628+BoomCheeseBurge@users.noreply.github.com>
2025-02-09 20:59:48 +0700

    Added navbar, and modified and styled auth and guest layout with DaisyUI

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,18 +1,23 @@
 import ApplicationLogo from '@/Components/Application/ApplicationLogo';
+import NavBar from '@/Components/Core/Navigation/NavBar';
 import { Link } from '@inertiajs/react';
 import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+        <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+            <NavBar />
+            
+            <div className='flex flex-col items-center pt-6 sm:justify-center sm:pt-20'>
+                <div>
+                    <Link href="/">
+                        <ApplicationLogo className="w-20 h-20 text-gray-500 fill-current" />
+                    </Link>
+                </div>
 
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">
-                {children}
+                <div className="w-full overflow-hidden px-6 py-4 mt-6 bg-white shadow-md dark:bg-gray-800 sm:max-w-md sm:rounded-lg">
+                    {children}
+                </div>
             </div>
         </div>
     );
